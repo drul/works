149b68264a7b5b9f0b4673aae1e220f2650028cd
drul <drul>
2020-02-12 11:39:26 +0300

    init

---
+++ auth.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+<form action="auth.php">
+    <label for="login">
+        <input type="text" id="login" name="login">
+    </label>
+    <label for="password">
+        <input type="password" id="password" name="password">
+    </label>
+
+</form>
